85fa71553fce5711d775614ba49690f21ebc4362
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2023-02-09 16:44:14 +0700

    translate & translate/core

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'test';
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('vi');
+    translate.use('vi');
+  }
+
+  onTranslate(code: string) {
+    this.translate.setDefaultLang(code);
+    this.translate.use(code);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,34 @@ import { AppComponent } from './app.component';
 import { ButtonComponent } from './button/button.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildrenComponent } from './children/children.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonComponent,
     ParentComponent,
-    ChildrenComponent
+    ChildrenComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // ngx-translate and the loader module
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
